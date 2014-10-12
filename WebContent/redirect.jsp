8183c307fef39b232c1264e4477b7aabe356e5e1
danny4zg <danijel.sostar@zg.t-com.hr>
2014-10-12 18:36:23 +0200

    Revert "SVIS - Dokumenti i obrasci"
    This reverts commit ba98a10a094a05c78409b7558f1a0b5a25dc6324.

--- WebContent/redirect.jsp
+++
@@ -1,5 +0,0 @@
-<% 
-	String path=request.getParameter("path");
-	System.out.println("JSP redirect path: " + path);
-	response.sendRedirect(request.getParameter("path")); 
-%>
--- src/hr/kingict/svis/reports/DSGenericInterface.java
+++
@@ -1,9 +0,0 @@
-package hr.kingict.svis.reports;
-
-import java.util.List;
-
-import net.sf.jasperreports.engine.JRDataSource;
-
-public interface DSGenericInterface extends JRDataSource {
-	public List<?> getList();
-}
